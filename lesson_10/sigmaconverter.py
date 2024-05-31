f5f7a1dade918a4e885a71ff89b29a81f6117cc3
CDHai <caoduyhai007@gmail.com>
2024-05-31 11:16:59 +0700

    update code still bug :D

--- lesson_10/sigmaconverter.py
+++ lesson_10/sigmaconverter.py
@@ -4,44 +4,45 @@ import pandas as pd
 import schedule
 import time
 
-def pull_sigma_rules(repo_url, repo_dir):
-    if os.path.exists(repo_dir):
-        subprocess.run(["git", "-C", repo_dir, "pull"], check=True)
+SIGMA_REPO_URL = "https://github.com/SigmaHQ/sigma.git"
+SIGMA_REPO_DIR = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/sigma"
+SIGMA_RULES_DIR = os.path.join(SIGMA_REPO_DIR, "rules", "windows", "raw_access_thread")
+OUTPUT_FILE = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/sigma_rule.txt"
+EXCEL_FILE = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/output.xlsx"
+
+def pull_sigma_rules():
+    if os.path.exists(SIGMA_REPO_DIR):
+        subprocess.run(["git", "-C", SIGMA_REPO_DIR, "pull"], check=True)
     else:
-        subprocess.run(["git", "clone", repo_url, repo_dir], check=True)
+        subprocess.run(["git", "clone", SIGMA_REPO_URL, SIGMA_REPO_DIR], check=True)
 
-def convert_sigma_to_splunk(sigma_dir, output_file):
-    sigmac_path = "C:/Users/admin/anaconda3/Scripts/sigmac.exe"  # Thay đổi nếu cần thiết
-    if not os.path.isfile(sigmac_path):
-        raise FileNotFoundError(f"{sigmac_path} không được tìm thấy")
-
-    with open(output_file, 'w') as f:
-        for root, dirs, files in os.walk(sigma_dir):
+def convert_sigma_to_splunk():
+    with open(OUTPUT_FILE, 'w') as f:
+        for root, _, files in os.walk(SIGMA_RULES_DIR):
             for file in files:
-                if file.endswith(".yml") or file.endswith(".yaml"):
-                    sigma_file_path = os.path.join(root, file)
-                    subprocess.run([sigmac_path, "convert", "-t", "splunk", sigma_file_path], stdout=f, check=True)
+                try:
+                    if file.endswith(".yml") or file.endswith(".yaml"):
+                        sigma_file_path = os.path.join(root, file)
+                        subprocess.run(["sigma", "convert", "--without-pipeline", "-t", "splunk", sigma_file_path], stdout=f, check=True)
+                except Exception as e:
+                    print("Error: ", e)
 
-def write_to_excel(data, excel_file):
+def write_to_excel():
+    with open(OUTPUT_FILE, 'r') as f:
+        data = [{"rule": line.strip(), "description": ""} for line in f]
     df = pd.DataFrame(data)
-    df.to_excel(excel_file, index=False)
+    df.to_excel(EXCEL_FILE, index=False)
 
 def job():
-    repo_url = "https://github.com/SigmaHQ/sigma.git"
-    repo_dir = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/sigma"
-    sigma_dir = os.path.join(repo_dir, "rules", "windows", "process_creation")
-    output_file = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/sigma_rule.txt"
-    excel_file = "C:/Users/admin/Desktop/Python-For-Security-Engineer/lesson_10/output.xlsx"
+    pull_sigma_rules()
+    convert_sigma_to_splunk()
+    write_to_excel()
 
-    pull_sigma_rules(repo_url, repo_dir)
-    convert_sigma_to_splunk(sigma_dir, output_file)
-    
-    with open(output_file, 'r') as f:
-        data = [{"rule": line.strip(), "description": ""} for line in f]
-    write_to_excel(data, excel_file)
+def main():
+    schedule.every(3).seconds.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-schedule.every(3).seconds.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    main()
